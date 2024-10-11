d6c9db77b75f5d339c6dc735f0439ce078a74d99
BrianGacheru8 <briangacheru255@gmail.com>
2024-10-11 08:36:33 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,21 +5,23 @@ img=document.querySelector(img)
 
 const handleClick = function(ramen){
   // Add code
-  fetch(http://localhost:3000)
+     fetch(http://localhost:3000)
         .then(res=>res.JSON())
-        .then(data=>const tree =data)
-  for (const ramen of ramens){
-     if (ramen["id"]=tree["id"]){
-      this.name=document.getElementbyClass(name)
-      this.restaurant=document.getElementbyClass(restaurant)
-      this.image=document.getElementbyClass(image)
-      this.rating=document.getElementbyClass(rating)
-      this.comment=document.getElementbyClass(comment)
-      this.id=document.getElementbyClass(id)
-      break}
+        .then(data=>const ramens =data)
+     ramens.forEach(      
+     for (ramen in ramens){      
+       if (ramen["id"]=tree["id"]){
+         tree.name=document.getElementbyClass(name)
+         tree.restaurant=document.getElementbyClass(restaurant)
+         tree.image=document.getElementbyClass(image)
+         tree.rating=document.getElementbyClass(rating)
+         tree.comment=document.getElementbyClass(comment)
+         tree.id=document.getElementbyClass(id)
+         break}else{
+         console.log("none")
    }    
   
-img.addEventListener("click",handleClick())    
+document.queryselector(img).addEventListener("click",handleClick())    
     
 const addSubmitListener = () => {
   // Add code
