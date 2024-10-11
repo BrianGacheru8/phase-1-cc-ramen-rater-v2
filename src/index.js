0d492a422b753449ea3df6177a23836074806888
BrianGacheru8 <briangacheru255@gmail.com>
2024-10-11 03:52:21 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ const addSubmitListener = () => {
 const displayRamens = () => {
   // Add code
   fetch(http://localhost:3000)
+        .then(res=>res.json())
+        .then(data=>console.log(data))
 };
 
 const main = () => {
