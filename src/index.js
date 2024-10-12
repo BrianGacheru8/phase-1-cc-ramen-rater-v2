ae586cd22c043206b0a4cb1e70078d212858fb05
BrianGacheru8 <briangacheru255@gmail.com>
2024-10-12 05:50:10 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ const addSubmitListener = () => {
   const form=document.getElementById("new-ramen")
   form.addEventListener("submit",()=>{
     fetch(("http://localhost:3000/ramens"),{
+          mode: 'cors',
+          credentials: 'include',
           method:"POST",
           headers:{"Content-type":"application/json"},
           body:json.stringify(form)})
